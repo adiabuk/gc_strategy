c9f81887fa95b759f66a541319e3f6820254bb1c
Amro Diab <adiab@hotmail.co.uk>
2022-05-15 21:39:03 +0100

    lower timeframes

---
+++ script.sh
@@ -0,0 +1,21 @@
+
+
+
+
+INPUT:
+year
+interval
+pair
+version (of gc)
+config file
+
+
+
+OUTPUT:
+# winning trades
+# losing trades
+# total trades
+# num trades per day
+# avg length of trade
+# report
+# graphs
